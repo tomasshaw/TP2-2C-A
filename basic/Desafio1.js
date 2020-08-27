1e4de84cb6affe7e7620eb7bf71cc7832e27da16
Thomas Shaw <tomasshaw@outlook.com>
2020-08-27 00:31:18 -0300

    feat: mvp

--- basic/Desafio1.js
+++ basic/Desafio1.js
@@ -21,6 +21,30 @@ const str = `<ul>
   <li data-time="3:31">Flexbox Video</li>
 </ul>`;
 
+const argv = process.argv[2]
+argv ||
+	console.info('---------------------------------------\nSe puede usar un argumento al correr este script.\nValores aceptados: Redux || Flexbox\n---------------------------------------\n')
+if (argv !== 'Redux' && argv !== 'Flexbox' && argv !== undefined) {
+	console.error('Parametro invalido')
+	return
+}
+
+const buscar = argv || 'Redux'
+const ReduxArray = []
+const s = str.split('\n')
+const a = s.map(line => {
+	const matched = line.match(buscar)
+	matched && ReduxArray.push(matched.input)
+})
+
+let totTime = 0
+ReduxArray.map(e => {
+	const time = e.split('"')[1].split(':')
+	totTime += time[0]*60 + time[1]*1
+})
+
+console.log(`El total de segundos de ${buscar} es: ${totTime}`)
+
 // obtener el total de segundos de todos los videos del tipo 'Redux Video'
 // pistas: convertirlo en objetos
 // Vale por el primer parcial
